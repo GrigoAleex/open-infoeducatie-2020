8a1b619ecb7cbfefde8d34bcb39432d0a98161b7
GrigoAleex <grigorescu.aleex@gmail.com>
2020-07-30 16:40:48 +0300

    Start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Video Chat</title>
+        <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+        <style>
+            * {
+                font-family: 'Oxygen', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="app">
+            <video-chat></video-chat>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
